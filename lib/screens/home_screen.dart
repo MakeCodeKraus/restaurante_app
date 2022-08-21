5fa8053a48d91aede7d5fa92ed460076897e84d9
MakeCodeKraus <estefania.lopez.varelas@gmail.com>
2022-08-21 13:51:37 -0500

    subir proyecto

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,307 @@
+import 'package:restaurante_app/consttants.dart';
+import 'package:restaurante_app/screens/descuentos.dart';
+import 'package:restaurante_app/screens/menu.dart';
+import 'package:restaurante_app/widgets/book_rating.dart';
+import 'package:restaurante_app/widgets/reading_card_list.dart';
+import 'package:restaurante_app/widgets/two_side_rounded_button.dart';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/menuprincipal.jpg"),
+                  alignment: Alignment.topCenter,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: size.height * .1),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: RichText(
+                      text: TextSpan(
+                        style: Theme.of(context).textTheme.displaySmall,
+                        children: [
+                          TextSpan(text: "¿Qué deseas probar \nel dia de "),
+                          TextSpan(
+                              text: "hoy?",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ))
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: <Widget>[
+                        ReadingListCard(
+                          image: "assets/images/comida.png",
+                          title: "Visualizar el menú",
+                          auth: "Elije lo que más te gusta",
+                          rating: 4.9,
+                          pressDetails: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return VisualizarMenu();
+                                },
+                              ),
+                            );
+                          },
+                        ),
+                        ReadingListCard(
+                          image: "assets/images/descuento.png",
+                          title: "Adquirir descuentos",
+                          auth: "Promociones exclusivas",
+                          rating: 4.8,
+                          pressDetails: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return Descuentos();
+                                },
+                              ),
+                            );
+                          },
+                        ),
+                        SizedBox(width: 30),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 24),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        RichText(
+                          text: TextSpan(
+                            style: Theme.of(context).textTheme.displaySmall,
+                            children: [
+                              TextSpan(text: "Conoce nuestras "),
+                              TextSpan(
+                                text: "instalaciones",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                        bestOfTheDayCard(size, context),
+                        RichText(
+                          text: TextSpan(
+                            style: Theme.of(context).textTheme.displaySmall,
+                            children: [
+                              TextSpan(text: "Contactanos para "),
+                              TextSpan(
+                                text: "domicilios y reservas",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Container(
+                          height: 80,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(38.5),
+                            boxShadow: [
+                              BoxShadow(
+                                offset: Offset(0, 10),
+                                blurRadius: 33,
+                                color: Color(0xFFD3D3D3).withOpacity(.84),
+                              ),
+                            ],
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(38.5),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Expanded(
+                                  child: Padding(
+                                    padding:
+                                        EdgeInsets.only(left: 30, right: 20),
+                                    child: Row(
+                                      children: <Widget>[
+                                        Expanded(
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.end,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: <Widget>[
+                                              Text(
+                                                "De manera fácil y eficiente",
+                                                style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                              Text(
+                                                "No lo dudes",
+                                                style: TextStyle(
+                                                  color: kLightBlackColor,
+                                                ),
+                                              ),
+                                              Align(
+                                                alignment:
+                                                    Alignment.bottomRight,
+                                                child: Text(
+                                                  "Asistencia Virtual",
+                                                  style: TextStyle(
+                                                    fontSize: 10,
+                                                    color: kLightBlackColor,
+                                                  ),
+                                                ),
+                                              ),
+                                              SizedBox(height: 5),
+                                            ],
+                                          ),
+                                        ),
+                                        Image.asset(
+                                          "assets/images/telefono.png",
+                                          width: 55,
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  height: 7,
+                                  width: size.width * .65,
+                                  decoration: BoxDecoration(
+                                    color: kProgressIndicator,
+                                    borderRadius: BorderRadius.circular(7),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 40),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container bestOfTheDayCard(Size size, BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 20),
+      width: double.infinity,
+      height: 245,
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: EdgeInsets.only(
+                left: 24,
+                top: 24,
+                right: size.width * .35,
+              ),
+              height: 230,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color(0xFFEAEAEA).withOpacity(.45),
+                borderRadius: BorderRadius.circular(29),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                    child: Text(
+                      "Medellín, 21 de Agosto 2022",
+                      style: TextStyle(
+                        fontSize: 9,
+                        color: kLightBlackColor,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "Conoce todas nuestras sedes",
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  Text(
+                    "Elige la más cercana",
+                    style: TextStyle(color: kLightBlackColor),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10, bottom: 10.0),
+                    child: Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(right: 10.0),
+                          child: BookRating(score: 4.9),
+                        ),
+                        Expanded(
+                          child: Text(
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 10,
+                              color: kLightBlackColor,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            right: 0,
+            top: 0,
+            child: Image.asset(
+              "assets/images/ubicacion.png",
+              width: size.width * .27,
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: SizedBox(
+              height: 40,
+              width: size.width * .3,
+              child: TwoSideRoundedButton(
+                text: "Ubicación",
+                radious: 24,
+                press: () {},
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/menu.dart
@@ -0,0 +1,349 @@
+import 'package:restaurante_app/consttants.dart';
+import 'package:restaurante_app/widgets/book_rating.dart';
+import 'package:restaurante_app/widgets/rounded_button.dart';
+import 'package:flutter/material.dart';
+
+class VisualizarMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Stack(
+              alignment: Alignment.topCenter,
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.topCenter,
+                    padding: EdgeInsets.only(
+                        top: size.height * .12,
+                        left: size.width * .1,
+                        right: size.width * .02),
+                    height: size.height * .48,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/fondo1.jpg"),
+                        fit: BoxFit.fitWidth,
+                      ),
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(50),
+                        bottomRight: Radius.circular(50),
+                      ),
+                    ),
+                    child: BookInfo(
+                      size: size,
+                    )),
+                Padding(
+                  padding: EdgeInsets.only(top: size.height * .48 - 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      ChapterCard(
+                        name: "Menú del día",
+                        opcion: 1,
+                        tag: "Núestro menú es actualizado día a día",
+                        press: () {},
+                      ),
+                      ChapterCard(
+                        name: "Ensaladas",
+                        opcion: 2,
+                        tag: "Para acompañar tu plato principal",
+                        press: () {},
+                      ),
+                      ChapterCard(
+                        name: "Postres",
+                        opcion: 3,
+                        tag: "No te quedes con el antojo",
+                        press: () {},
+                      ),
+                      ChapterCard(
+                        name: "Bebidas",
+                        opcion: 4,
+                        tag: "Contamos con gran variedad",
+                        press: () {},
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  RichText(
+                    text: TextSpan(
+                      style: Theme.of(context).textTheme.headline5,
+                      children: [
+                        TextSpan(
+                          text: "Agregar al ",
+                        ),
+                        TextSpan(
+                          text: "carrito de compras.",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Stack(
+                    children: <Widget>[
+                      Container(
+                        height: 180,
+                        width: double.infinity,
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          padding:
+                              EdgeInsets.only(left: 24, top: 24, right: 150),
+                          height: 160,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(29),
+                            color: Color(0xFFFFF8F9),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              RichText(
+                                text: TextSpan(
+                                  style: TextStyle(color: kBlackColor),
+                                  children: [
+                                    TextSpan(
+                                      text:
+                                          "Revisa muy bien tu carrito \nantes de realizar tu compra \n",
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: "Su pedido es el siguiente ...",
+                                      style: TextStyle(color: kLightBlackColor),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  BookRating(
+                                    score: 4.9,
+                                  ),
+                                  SizedBox(width: 10),
+                                  Expanded(
+                                    child: RoundedButton(
+                                      text: "Realizar compra",
+                                      verticalPadding: 10,
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: Image.asset(
+                          "assets/images/carrito.png",
+                          width: 150,
+                          fit: BoxFit.fitWidth,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChapterCard extends StatelessWidget {
+  final String name;
+  final String tag;
+  final int opcion;
+  final Function press;
+  const ChapterCard({
+    Key key,
+    this.name,
+    this.tag,
+    this.opcion,
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+      margin: EdgeInsets.only(bottom: 16),
+      width: size.width - 48,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(38.5),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, 10),
+            blurRadius: 33,
+            color: Color(0xFFD3D3D3).withOpacity(.84),
+          ),
+        ],
+      ),
+      child: Row(
+        children: <Widget>[
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "Opción $opcion : $name \n",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: kBlackColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextSpan(
+                  text: tag,
+                  style: TextStyle(color: kLightBlackColor),
+                ),
+              ],
+            ),
+          ),
+          Spacer(),
+          IconButton(
+            icon: Icon(
+              Icons.arrow_forward_ios,
+              size: 18,
+            ),
+            onPressed: press,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BookInfo extends StatelessWidget {
+  const BookInfo({
+    Key key,
+    this.size,
+  }) : super(key: key);
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Flex(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        direction: Axis.horizontal,
+        children: <Widget>[
+          Expanded(
+              flex: 1,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Restaurantelv",
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline4
+                          .copyWith(fontSize: 28),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: this.size.height * .005),
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.only(top: 0),
+                    child: Text(
+                      "nuestro Menú",
+                      style: Theme.of(context).textTheme.subtitle1.copyWith(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            width: this.size.width * .3,
+                            padding:
+                                EdgeInsets.only(top: this.size.height * .02),
+                            child: Text(
+                              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                              maxLines: 5,
+                              style: TextStyle(
+                                fontSize: 10,
+                                color: kLightBlackColor,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin:
+                                EdgeInsets.only(top: this.size.height * .015),
+                            padding: EdgeInsets.only(left: 10, right: 10),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: FlatButton(
+                              onPressed: () {},
+                              child: Text(
+                                "¡Buen provecho!",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          IconButton(
+                            icon: Icon(
+                              Icons.favorite_border,
+                              size: 20,
+                              color: Colors.grey,
+                            ),
+                            onPressed: () {},
+                          ),
+                          BookRating(score: 4.9),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              )),
+          Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.transparent,
+                child: Image.asset(
+                  "assets/images/comida.png",
+                  height: double.infinity,
+                  alignment: Alignment.topRight,
+                  fit: BoxFit.fitWidth,
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+}
